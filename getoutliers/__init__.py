31d72cdabc8772f36522eb36983de14daf7a86f2
BidjorySamuel <bidjorys@gmail.com>
2024-07-06 18:44:36 -0300

    The getouliers has been published

---
+++ getoutliers/__init__.py
@@ -0,0 +1,16 @@
+"""
+
+get out liers😅
+====
+
+Outliers it's just a library to identify outliers in a pandas Dataframe and manipulate them
+
+'Made by Samuel Bidjory'
+
+github: https://github.com/BidjorySamuel/BidjorySamuel
+"""
+
+__version__ = "0.0.1"
+
+from getoutliers.__iqr import IQR
+from getoutliers.__outlier import Outlier_manipulater
---
+++ getoutliers/__outlier.py
@@ -0,0 +1,16 @@
+"""
+If anyone who whant to contribute in this module, go ahead!
+
+It's the more revelant module, cause that can manipulate a outliers in a pandas dataframe
+
+"""
+from numpy import ndarray, array
+
+#TODO
+class Outlier_manipulater():
+    def __str__(self, data:ndarray):
+        self.data = array(data)
+
+
+    def remove_outlier(self):
+        pass
