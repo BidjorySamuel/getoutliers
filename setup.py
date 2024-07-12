6da17bc8ab809d1db0a520d051a5ffe6b6df5860
BidjorySamuel <bidjorys@gmail.com>
2024-07-12 13:45:25 -0300

    nothing great

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="getoutliers",
-    version="0.0.4",
-    packages=find_packages(where="getoutliers"),
-    package_dir={"":"getoutliers"},
+    version="0.0.5.1",
+    packages=find_packages(),
+    url="https://github.com/BidjorySamuel/getoutliers",
+    author_email="bidjorysamuel@gmail.com",
 
     # This package is based on numpy and pandas, so it's really necessary that you install it
 
@@ -16,10 +17,15 @@ setup(
         "matplotlib",
     ],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT",
-        "Operating System :: OS Independent",
-    ],
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    # Adicione outras classificações conforme necessário
+],
 
 )
 
